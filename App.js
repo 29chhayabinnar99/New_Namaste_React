257411aaf3f9f64b25e3c379b4a8bbf7bdc61c5e
29chhayabinnar99 <chhayabinnar2910@gmail.com>
2026-02-23 15:36:16 +0530

    episode_03

--- App.js
+++ App.js
@@ -1,43 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/** CREATE SINGLE COMPONENT */
-//heading is a object if u print it in console this will creat element h1
-//{}this is a object where we can give attribute to the h1 tag sometimes we have to give class id or anything
-const heading = React.createElement(
-  "h1",
-  { id: "heading" }, // this are the props
-  "Hello world in React", //this is a children
-);
-// this will put h1 element in the root id div
+//creating element with help of react
+// React.createElement =>  React element (JS object) → ReactDOM renders  => real HTML element in browser
+const heading = React.createElement("h1", { id: "heading" }, "Namste React 🚀");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root will render the heading
 root.render(heading);
 
-/** THIS IS NESTED COMPONENT CREATION */
-/**
- *
- * <div id="parent">
- *      <div id="child">
- *          <h1> I am H1 tag</h1>
- *          //If you want to create sibling tags you have to create an array of children
- *          <h2> I am H1 tag</h2>
- *      </div>
- * </div>
- */
+// creating element with help of react
+// jsx => babel transpile => React.createElement => React element (JS object) → ReactDOM renders  => real HTML element in browser
+const heading2 = <h2 id="heading2">Namaste JSX 🚀</h2>;
+const jsxRoot = ReactDOM.createRoot(document.getElementById("jsxRoot"));
+jsxRoot.render(heading2);
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am H1 tag"),
-    React.createElement("h2", {}, "I am H2 tag"),
-  ]),
+//component - reusable piece of UI. It can be nested, reused, and composed. there are two types of component
+// funtional component (new way)
+// class- based component (old way)
+
+//creating funtional component
+const FunctionalComponent = () => {
+  return <h1 id="functionalHeading">Namaste React Functional Component 🚀</h1>;
+};
+const functionalRoot = ReactDOM.createRoot(
+  document.getElementById("functionalRoot"),
 );
+functionalRoot.render(<FunctionalComponent />);
 
-//we store parent-child id into parentchild veriable or viseversa
-const parentChild = ReactDOM.createRoot(
-  document.getElementById("parent-child"),
-);
+//Assignment
+function Assignment() {
+  return <div id="AssignmentHeading">hi</div>;
+}
 
-parentChild.render(parent);
+const AssignRoot = ReactDOM.createRoot(document.getElementById("AssignRoot"));
+AssignRoot.render(<Assignment />);
